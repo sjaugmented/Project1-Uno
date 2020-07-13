587ace7e8b1e4df4838aaf0dcac844773cfb2292
sjaugmented <sjaugmented@gmail.com>
2020-07-13 16:36:48 -0700

    added shuffleDeck

--- js/app.js
+++ js/app.js
@@ -11,18 +11,13 @@ const playPileDom = document.querySelector('.play-pile')
 const drawPileDom = document.querySelector('.draw-pile')
 
 class Card {
-    constructor(color, value, points, changeTurn, drawValue, instance, imgSrc) {
+    constructor(color, value, points, changeTurn, drawValue, imgSrc) {
         this.color = color
         this.value = value
         this.point = points
         this.changeTurn = changeTurn
         this.drawValue = drawValue
-        this.instances = instance
         this.src = imgSrc
-    }
-    circulateCard() {
-        if (instances > 0) instances--
-        else console.log(`There are no more ${this.color} ${this.value}.`)
     }
 }
 
@@ -30,24 +25,27 @@ function createCard(color) {
     for (let n = 0; n <= 14; n++) {
         // number cards
         if (n === 0) {
-            deck.push(new Card(color, n, n, true, 0, 1, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, n, true, 0, 'images/' + color + n + '.png'))
         }
         else if (n > 0 && n <= 9) {
-            deck.push(new Card(color, n, n, true, 0, 2, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, n, true, 0, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, n, true, 0, 'images/' + color + n + '.png'))
         }
         // reverse/skip
         else if (n === 10 || n === 11) {
-            deck.push(new Card(color, n, 20, false, 0, 2, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, 20, false, 0, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, n, true, 0, 'images/' + color + n + '.png'))
         }
         // draw 2
         else if (n === 12) {
-            deck.push(new Card(color, n, 20, true, 2, 2, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, 20, true, 2, 'images/' + color + n + '.png'))
+            deck.push(new Card(color, n, n, true, 0, 'images/' + color + n + '.png'))
         }
         else if (n === 13) {
-            deck.push(new Card('any', n, 50, true, 0, 1, 'images/wild' + n + '.png'))
+            deck.push(new Card('any', n, 50, true, 0, 'images/wild' + n + '.png'))
         }
         else {
-            deck.push(new Card('any', n, 50, true, 4, 1, 'images/wild' + n + '.png'))
+            deck.push(new Card('any', n, 50, true, 4, 'images/wild' + n + '.png'))
         }
     }
 }
@@ -73,6 +71,15 @@ const createDeck = () => {
     }
 
     console.log(deck)
+}
+
+function shuffleDeck(array){
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    let temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
 }
 
 
