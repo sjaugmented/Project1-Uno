a1623ce1edeeada7b932cd79b0d9050b82da9769
sjaugmented <sjaugmented@gmail.com>
2020-07-13 17:19:44 -0700

    deal cards to player and cpu and set first play card that isn't an action card

--- js/app.js
+++ js/app.js
@@ -10,6 +10,9 @@ const playerScoreDom = document.querySelector('#player-score')
 const playPileDom = document.querySelector('.play-pile')
 const drawPileDom = document.querySelector('.draw-pile')
 
+cpuHand = []
+playerHand = []
+
 class Card {
     constructor(color, value, points, changeTurn, drawValue, imgSrc) {
         this.color = color
@@ -54,7 +57,9 @@ function createCard(color) {
 const deck = []
 
 const createDeck = () => {
-    // create everything except wild
+    // destroy previous deck
+    deck.length = 0
+    // create new deck
     for (let i = 0; i <= 3; i++){
         if (i === 0) {
             createCard('red')
@@ -73,19 +78,61 @@ const createDeck = () => {
     console.log(deck)
 }
 
-function shuffleDeck(array){
-  for (let i = array.length - 1; i > 0; i--) {
+function shuffleDeck(){
+  for (let i = deck.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1))
-    let temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
+    let temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
   }
+}
+
+function dealCards() {
+    for (let i = 0; i < 7; i++) {
+        // deal cards into cpu/player arrays
+        cpuHand.push(deck.shift())       
+        playerHand.push(deck.shift())
+
+        // put cards on the DOM
+        const cpuCard = document.createElement('img')
+        cpuCard.setAttribute('src', '/images/back.png')
+        cpuHandDom.appendChild(cpuCard)
+
+        const playerCard = document.createElement('img')
+        playerCard.setAttribute('src', playerHand[i].src)
+        playerHandDom.appendChild(playerCard)
+    }
+}
+
+function startPlayPile() {
+    // set first play card to first card in the deck that is a number card
+    const playCard = document.createElement('img')
+    for (let i = 0; i < deck.length; i++) {
+        if (deck[i].value < 10) {
+            playCard.setAttribute('src', deck[i].src)
+            break
+        } 
+    }
+    playPileDom.appendChild(playCard)
 }
 
 
 const newHand = () => {
+    // clear hands and play pile
     cpuHandDom.innerHTML = ''
+    cpuHand.length = 0
     playerHandDom.innerHTML = ''
+    playerHand.length = 0
+    playPileDom.innerHTML = ''
+
+    // create new deck
+    createDeck()
+    // shuffle deck
+    shuffleDeck()
+    // deal cards and first play card
+    dealCards()
+    // set down first play card that isn't an action card
+    startPlayPile()
 }
 
 
